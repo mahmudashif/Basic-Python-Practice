4d518d1cc415dce151ff5e0943b33c2770dfe8a2
ashifmahmud <mdamahmud2000@gmail.com>
2024-03-12 00:32:13 +0600

    bee 1001 edit

---
+++ bee1001.py
@@ -0,0 +1,11 @@
+# Take user input for variable A
+A = int(input())
+
+# Take user input for variable B
+B = int(input())
+
+# Add the variables and store the result in a third variable X
+X = A + B
+
+# Print the result with an endline
+print("X =", X)
