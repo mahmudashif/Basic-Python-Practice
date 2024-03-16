f0289d1db59850c40b164c9f83875e46b3ea00cf
ashifmahmud <mdamahmud2000@gmail.com>
2024-03-16 19:34:27 +0600

    Apply condition is successful

---
+++ apply.py
@@ -0,0 +1,7 @@
+cgpa = float(input("Enter your CGPA : "))
+if(cgpa>5):
+    print("This is unvalid CGPA")
+elif(cgpa==5):
+    print("Congo, You can Apply")
+else:
+    print("Better Luck for next time")
